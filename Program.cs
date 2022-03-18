67e3f32455c45c4bd3a0a058e8ef0b83df3bb1ac
Luis Antonio <luisengenhariaeletronica@gmail.com>
2022-03-18 13:37:47 -0300

    Soma de dois numeros

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Visual
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, soma;
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            soma = a + b;
+            Console.WriteLine(soma);
+        }
+    }
+}
