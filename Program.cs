6065021f8297ca3ada5158e692f9463322469709
Luis22Ant <luisengenhariaeletronica@gmail.com>
2022-10-05 15:33:12 -0300

    SUBTRAÇÃO

--- Program.cs
+++ Program.cs
@@ -8,12 +8,6 @@
             s = int.Parse(Console.ReadLine());
             c = int.Parse(Console.ReadLine());
             soma = a - b;
-            Console.WriteLine(soma);
-            int s, c, soma;
-            s = int.Parse(Console.ReadLine());
-            c = int.Parse(Console.ReadLine());
-            soma = a + b;
-            Console.WriteLine(soma);
         }
     }
 }
