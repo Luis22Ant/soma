0a598b4ca3d8a09c1bbe70b712aa97710572541e
Antonio <88328310+guardian-dev-005@users.noreply.github.com>
2023-01-26 17:19:39 -0300

    Teste

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@
     {
         static void Main(string[] args)
         {
+        
             int a, b, soma;
             a = Convert.ToInt32(Console.ReadLine());
             b = Convert.ToInt32(Console.ReadLine());
