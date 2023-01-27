e0830905c13f76c310c7a6ed3a26c986a29aa465
Luis22Ant <luisengenhariaeletronica@gmail.com>
2023-01-27 16:40:00 -0300

    made changes

--- Program.cs
+++ Program.cs
@@ -10,8 +10,7 @@
             b = Convert.ToInt32(Console.ReadLine());
             soma = a+b+a-b;
             sub=a+b-b;
-            //funciounou
-            //tropa do ainda
+            //acho que deu bom rapaziada
         }
     }
 }
