4d060e9944dbd447684c9d9b746a30e9cb1ea12d
Luis22Ant <luisengenhariaeletronica@gmail.com>
2023-01-27 16:25:17 -0300

    made changes

--- Program.cs
+++ Program.cs
@@ -5,10 +5,11 @@
         static void Main(string[] args)
         {
         
-            int a, b, soma;
+            int a, b, soma,sub;
             a = Convert.ToInt32(Console.ReadLine());
             b = Convert.ToInt32(Console.ReadLine());
             soma = a+b+a-b;
+            sub=a-b;
         }
     }
 }
