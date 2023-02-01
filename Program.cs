db3aee0f3a62f4e5c42eb8297697483552ed42d8
Luis22Ant <luisengenhariaeletronica@gmail.com>
2023-02-01 14:24:58 -0300

    Auto push

--- Program.cs
+++ Program.cs
@@ -9,11 +9,7 @@
             a = Convert.ToInt32(Console.ReadLine());
             b = Convert.ToInt32(Console.ReadLine());
             soma = a+b+a+b;
-            //acho que deu bom rapaziada
-           //BarbosaMeDeve1000Reais
-           //LBeB
-           //FalaRaviloli
-           //TurzinKDminhaCervejaArtesanal?
+           
             
         }
     }
